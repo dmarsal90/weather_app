d89af0449e8c1ec10956b1955eb0dde8f93b73ee
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-08 16:02:46 -0500

    Update api component

---
+++ src/assets/components/api.jsx
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'fc71af6444msh60c45a666bf303bp16bb74jsn54e9a97fd365',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo";
