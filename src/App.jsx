1ce7cf0fca9415812f7de3402d1143d1e33a2551
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-08 16:19:29 -0500

    Add current-weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 import './App.css'
 import {Search} from "./assets/components/search/Search";
+import {CurrentWeather} from "./assets/components/current-weather/current-weather";
 
 function App() {
-  return (
-    <div className="container">
-     <Search/>
-    </div>
-  )
+    return (
+        <div className="container">
+            <Search/>
+            <CurrentWeather/>
+        </div>
+    )
 }
 
 export default App
