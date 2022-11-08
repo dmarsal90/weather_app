4f79b22fd1f028b3bb71d1f23bddf514eec4a00e
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-08 17:12:07 -0500

    Add weather constants

--- src/assets/components/api.jsx
+++ src/assets/components/api.jsx
@@ -7,3 +7,6 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+export const WEATHER_API_KEY = "d64e06966e1bd55dc36dd493eea10a98";
