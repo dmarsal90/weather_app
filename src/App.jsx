988f6c0f625cf271a058c296a428f6df99076969
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-08 22:58:32 -0500

    Update App

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ function App() {
     const handleOnSearchChange = (searchData) => {
         const [lat, lon] = searchData.value.split(" ");
 
-        const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+        const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
-        const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`);
+        const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
 
         Promise.all([currentWeatherFetch, forecastFetch]).then(async (response) => {
             const weatherResponse = await response[0].json();
