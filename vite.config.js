d10a0507e5f3e009efba7a51a5616383148cc972
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-11 15:08:09 -0500

    Add vitest

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './tests/setup.js',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test/setup.jsx',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
     }
+  }
 })
